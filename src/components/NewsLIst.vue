8fb88d36903d136ef5dacc7555d79292cbd9c19a
Raffique <raffiquemuir@yahoo.com>
2022-04-04 16:27:26 -0500

    final

--- src/components/NewsLIst.vue
+++ src/components/NewsLIst.vue
@@ -1,9 +1,18 @@
 <template>
+
+    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+        <div class="input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <button class="btn btn-primary mb-2">Search</button>
+        </div>
+        <p>You are searching for {{ searchTerm }}</p>
+    </form>
     <div class="container-fuild">
         
             <div v-for="article in articles" :key="article" style="display: inline-block; margin: 10px;" >
                 <div class="card" style="width:300px; height: 480px;">
-                    <img class="card-img-top" style="width: 300px; height: 200px;" :src="`${article.urlToImage}`" alt="Card image">
+                    <img  style="width: 300px; height: 200px;" :src="`${article.urlToImage}`" alt="Card image">
                     
                         <h5 class="card-title">{{ article.title }}</h5>
 
@@ -13,10 +22,6 @@
                </div>
             </div>
             
-            <!--
-                <img style="width: 300px; height: 300px; margin: 10px;" alt="image" src="{{ url_for('get_image', filename=file.img) }}" /></li>
-            -->
-        
     </div>
 </template>
 
@@ -24,8 +29,26 @@
     export default {
         data() {
             return {
-                articles: []
+                articles: [],
+                searchTerm: ''
             };
+        },
+        methods: {
+            searchNews() {
+                let self = this;
+                fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en', {
+                    headers: {
+                    'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+                    }
+                })
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    self.articles = data.articles;
+                });
+            }
         },
         created() {
 
